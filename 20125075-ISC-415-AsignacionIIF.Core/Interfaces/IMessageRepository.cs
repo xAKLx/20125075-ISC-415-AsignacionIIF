c9d044904790b97a910cdedb3f6f98a2f7e90f88
Angel Guzman <angelguzmaning@hotmail.com>
2016-07-16 14:21:48 -0400

    Add Core project
    The core project will manage the business objects and the interfaces of the database.

---
+++ 20125075-ISC-415-AsignacionIIF.Core/Interfaces/IMessageRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20125075_ISC_415_AsignacionIIF.Core.Interfaces
+{
+    interface IMessageRepository
+    {
+        void Add(Message m);
+
+        IEnumerable GetMessages();
+
+        ICollection<Message> Find(String sender, String receiver);
+    }
+}
---
+++ 20125075-ISC-415-AsignacionIIF.Core/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20125075_ISC_415_AsignacionIIF.Core
+{
+    public class Message
+    {
+        [Required]
+        [MinLength(3)]
+        public String Sender { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        public String Receiver { get; set; }
+
+        [Required]
+        public String Content { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+    }
+}
